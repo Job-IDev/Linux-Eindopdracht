0c50341bb36974b47b688056a1b5fbe376f43972
Job Scholten <cooljob.js@gmail.com>
2021-06-15 12:47:46 +0200

    spelfout in bootstrap

--- SaltMinionInit.sh
+++ SaltMinionInit.sh
@@ -1,5 +1,5 @@
 # Dit is het script om de SaltMinions te installeren.
 
-curl -L https://bootstap.saltstack.com -o install_salt.sh
+curl -L https://bootstrap.saltstack.com -o install_salt.sh
 sudo sh install_salt.sh
 sudo sh -c "echo '10.0.25.36 salt' >> /etc/hosts"
