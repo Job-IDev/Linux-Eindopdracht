05f638c3b00b91257dd9346400902e9f52d96689
Job Scholten <cooljob.js@gmail.com>
2021-06-15 12:21:19 +0200

    Wijziging naar bootstrap

--- SaltMasterInit.sh
+++ SaltMasterInit.sh
@@ -1,9 +1,10 @@
 # Dit zijn de commando's die ik gebruikt heb in de SaltMaster om deze in te stellen als SaltMaster.
 
-apt-get update
-apt-get upgrade
+sudo apt-get update
+sudo apt-get upgrade
 
-apt-get install salt-master salt-minion
-hostname salt
+sudo apt-get install salt-master salt-minion
+sudo hostname salt
 
-wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+curl -L https://bootstrap.saltstack.com -o install_salt.sh
+sudo sh install_salt.sh
