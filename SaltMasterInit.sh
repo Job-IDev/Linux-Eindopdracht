7ad25bb94431611045ad6911c9f7f5f87617ba65
Job Scholten <cooljob.js@gmail.com>
2021-06-15 12:32:36 +0200

    -y toevoegen voor de acceptatie voor upgrade

--- SaltMasterInit.sh
+++ SaltMasterInit.sh
@@ -1,7 +1,7 @@
 # Dit zijn de commando's die ik gebruikt heb in de SaltMaster om deze in te stellen als SaltMaster.
 
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 sudo apt-get install salt-master salt-minion
 sudo hostname salt
