412058525b69e18a0490ddf8736edc7478f8c1de
Job Scholten <cooljob.js@gmail.com>
2021-06-15 12:41:31 +0200

    sudo toevoegen voor rechten

--- SaltMinionInit.sh
+++ SaltMinionInit.sh
@@ -1,5 +1,5 @@
 # Dit is het script om de SaltMinions te installeren.
 
 curl -L https://bootstap.saltstack.com -o install_salt.sh
-sh install_salt.sh
-sh -c "echo '10.0.25.36 salt' >> /etc/hosts"
+sudo sh install_salt.sh
+sudo sh -c "echo '10.0.25.36 salt' >> /etc/hosts"
